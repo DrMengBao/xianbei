bcac94ad611784e5f2ca6243e9bac8b2774386a8
Xiaohuang257 <Xiaohuang257@qq.com>
2022-01-24 17:43:46 +0800

    📄替换与🗑️删除

--- static/index.js
+++ static/index.js
@@ -341,11 +341,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
-    return '人？';
+    if (score <= 49) return '逊呐';
+    if (score <= 99) return '哼哼哼';
+    if (score <= 149) return '你是一个一个一个';
+    if (score <= 199) return '哼哼哼啊啊啊啊啊';
+    return '皮燕都撅烂了罢（恼';
 }
 
 function toStr(obj) {
